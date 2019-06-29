ffd921a2cade02f3551fe01cc7895dd39705506a
Parth <parthsolanki.solanki@gmail.com>
2019-06-29 16:48:35 -0400

    made sign-in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignOnComponent } from './sign-on/sign-on.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignOnComponent
   ],
   imports: [
     BrowserModule
